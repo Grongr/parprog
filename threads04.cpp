ac7949e70e59f14980456b10e5f0578cfcfb2111
Egor Osipov <e.a.osipov@mail.ru>
2022-03-16 04:40:05 +0300

    You finally awake

--- threads04.cpp
+++ threads04.cpp
@@ -1,51 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/types.h>
 
-pthread_mutex_t m;
-double result = 0;
+#define MAXIMUM_THREADS_COUNT 8
+
+pthread_mutex_t mut;
+
+typedef struct Vectors {
+
+    double result;
+
+    double* vec1;
+    double* vec2;
+
+    size_t n;
+
+    size_t step;
+
+    size_t step_id;
+
+} Vectors;
+
+void VectorsInit(Vectors* vec, size_t n, size_t m) {
+
+    vec->result  = 0;
+    vec->step_id = 0;
+    vec->n       = n;
+
+    vec->vec1 = (double*)calloc(n, sizeof(double));
+    vec->vec2 = (double*)calloc(n, sizeof(double));
+
+    vec->step = n / m;
+}
+
+void VectorsDestructor(Vectors* vec) {
+
+    free(vec->vec1);
+    free(vec->vec2);
+}
 
 void* dex(void* value) {
 
-    double* v = (double*) value;
+    Vectors* v = (Vectors*) value;
 
-    pthread_mutex_lock(&m);
+    pthread_mutex_lock(&mut);
 
-    result += v[0] * v[1];
+    size_t i = v->step_id * v->step;
+    for (i = v->step_id * v->step; i < (v->step_id + 1) * v->step; ++i)
+        v->result += v->vec1[i] * v->vec2[i];
 
-    pthread_mutex_unlock(&m);
+    if (i + v->step > v->n)
+        for (; i < v->n; ++i)
+            v->result += v->vec1[i] * v->vec2[i];
+    printf("Step: %ld    Result: %lg\n", v->step_id, v->result);
+
+    ++v->step_id;
+
+    pthread_mutex_unlock(&mut);
 
     return 0;
 }
 
-void solvetion(double* vecs, int n, pthread_t* thid, int m) {
+void solvetion(Vectors* vecs, pthread_t* thid, size_t m) {
 
-    for (int i = 0; i < n * 2; i += 2)
-        scanf("%lg", vecs + i);
+    for (int i = 0; i < vecs->n; ++i)
+        scanf("%lg", &vecs->vec1[i]);
 
-    for (int i = 1; i < n * 2; i += 2)
-        scanf("%lg", vecs + i);
+    for (int i = 0; i < vecs->n; ++i)
+        scanf("%lg", &vecs->vec2[i]);
 
-    for (int i = 0; i < n * 2; i += 2) {
+    for (int i = 0; i < m; ++i) {
 
-        int result = pthread_create(thid + i / 2, NULL, dex, (void*)(&vecs[i]));
+        /* dex( (void*)vecs ); */
 
-        if (result) {
+        int err = pthread_create(&thid[vecs->step_id], NULL, dex, (void*)vecs);
 
-            printf("Some errors with threads!!!\n");
+        if (err) {
+
+            printf("Thread creation error occured!!!\n");
             exit(-1);
         }
     }
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < m; ++i)
         pthread_join(thid[i], NULL);
-    }
-
-    printf("The result is: %8.8lg\n", result);
-
 }
 
 int main() {
@@ -57,12 +99,23 @@ int main() {
     scanf("%ld", &m);
 
     if (m > 8)
-        m = 8;
+        m = MAXIMUM_THREADS_COUNT;
 
-    double* vecs = (double*)calloc(n * 2, sizeof(double));
+    if (m > n)
+        m = n;
+
+    Vectors vecs;
+
+    VectorsInit(&vecs, n, m);
+
+    pthread_mutex_init(&mut, NULL);
 
     pthread_t* thid = (pthread_t*)calloc(m, sizeof(pthread_t));
+
+    solvetion(&vecs, thid, m);
+
+    printf("The result of multiplying is: %8.8lg\n", vecs.result);
     
-    
+    VectorsDestructor(&vecs);
     return 0;
 }
